c7e559888eeac2334d2efbfbfabff7efb06c4e18
Zeeshan Hassan <zishon89us@hotmail.com>
2016-03-05 20:24:26 +0500

    delete aws s3 object and bucket

--- aws/s3/create_bucket.js
+++ aws/s3/create_bucket.js
@@ -6,41 +6,79 @@
 var AWS = require('aws-sdk');
 
 //Load the S3 information from the environment variables.
-var AWS_ACCESS_KEY = "xxxAJ76xxxxxxxxx";//process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = "xxxxxxZrkJbTxxxxxxxxxxxxxxxxxxxxxx";//process.env.AWS_SECRET_KEY;
+/*var AWS_ACCESS_KEY = "xxxAJ76xxxxxxxxx";//process.env.AWS_ACCESS_KEY;
+ var AWS_SECRET_KEY = "xxxxxxZrkJbTxxxxxxxxxxxxxxxxxxxxxx";//process.env.AWS_SECRET_KEY;*/
 
-AWS.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
+AWS.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
 
 // Set your region for future requests.
 //AWS.config.region = 'us-west-2';
 
-// Create a bucket using bound parameters and put something in it.
-var s3bucket = new AWS.S3({params: {Bucket: 'random_bucket_name'}});
-s3bucket.createBucket(function(e,d) {
- if(!e){
-  var params = {Key: 'this_is_key', Body: 'This is data.'};
-  s3bucket.upload(params, function(err, data) {
-   if (err) {
-    console.log("Error uploading data: ", err);
-   } else {
-    console.log("Successfully uploaded.");
-   }
-  });
- }else{
-  console.log('Error Creating Bucket: ' + e);
- }
+//------------------------------------------------------
+//Create a bucket
+//Web Link=> http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#createBucket-property
+//------------------------------------------------------
 
-});
+/*var s3bucket = new AWS.S3({params: {Bucket: 'random_bucket_name'}});
+ s3bucket.createBucket(function (e, d) {
+ if (!e) {
+ var params = {Key: 'this_is_key', Body: 'This is data.'};
+ s3bucket.upload(params, function (err, data) {
+ if (err) {
+ console.log("Error uploading data: ", err);
+ } else {
+ console.log("Successfully uploaded.");
+ }
+ });
+ } else {
+ console.log('Error Creating Bucket: ' + e);
+ }
+ });*/
+
+//------------------------------------------------------
+//Delete bucket Note: All objects in bucket should be DELETED before bucket is deleted
+//http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteBucket-property
+//------------------------------------------------------
+/*
+ var s3 = new AWS.S3();
+ var params = {
+ Bucket: 'random_bucket_name'  //required
+ };
+ s3.deleteBucket(params, function(err, data) {
+ if (err) console.log(err, err.stack); // an error occurred
+ else     console.log(data);           // successful response
+ });*/
+
+//------------------------------------------------------
+//Delete bucket Note: All objects in bucket should be DELETED before bucket is deleted
+//http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteBucket-property
+//------------------------------------------------------
+/*
+ var s3 = new AWS.S3();
+ var params = {
+ Bucket: 'random_bucket_name', *//* required *//*
+ Key: 'this_is_key' *//* required *//*
+ };
+ s3.deleteObject(params, function(err, data) {
+ if (err) console.log(err, err.stack); // an error occurred
+ else     console.log(data);           // successful response
+ });*/
+
+
 //------------------------------------------------------
 //list all buckets
+//http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listBuckets-property
 //------------------------------------------------------
-var s3 = new AWS.S3();
-s3.listBuckets(function(err, data) {
- if (err) { console.log("Error:", err); }
- else {
-  for (var index in data.Buckets) {
-   var bucket = data.Buckets[index];
-   console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
-  }
+/*
+ var s3 = new AWS.S3();
+ s3.listBuckets(function (err, data) {
+ if (err) {
+ console.log("Error:", err);
  }
-});
+ else {
+ for (var index in data.Buckets) {
+ var bucket = data.Buckets[index];
+ console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+ }
+ }
+ });*/
