007094f5e90021db1d1f60a9bb929f516b31e039
Zeeshan Hassan <zishon89us@hotmail.com>
2016-03-06 12:53:23 +0500

    read multiple files alternate using node module read-multiple-files module

--- files/read_dir_files.js
+++ files/read_dir_files.js
@@ -1,16 +1,41 @@
 var fs = require('fs'),
-    async = require('async');
+    async = require('async'),
+    readMultipleFiles = require('read-multiple-files');
 
 var dirPath = 'sample_files/';
 
+//------------------------------------------------------
+//fs module to read multiple files using async
+//Web Link=>
+//------------------------------------------------------
+
+
 fs.readdir(dirPath, function (err, filesPath) {
     if (err) throw err;
-    filesPath = filesPath.map(function(filePath){
+    filesPath = filesPath.map(function (filePath) {
         return dirPath + filePath;
     });
-    async.map(filesPath, function(filePath, cb){
+    async.map(filesPath, function (filePath, cb) {
         fs.readFile(filePath, 'utf8', cb);
-    }, function(err, results) {
+    }, function (err, results) {
+        console.log(results);
+    });
+});
+
+//------------------------------------------------------
+//read-multiple-files
+//Web Link=> http://stackoverflow.com/questions/35823727/returning-the-content-of-multiple-files-in-node-js/35824248#35824248
+//------------------------------------------------------
+
+
+fs.readdir(dirPath, function (err, filesPath) {
+    if (err) throw err;
+    filesPath = filesPath.map(function (filePath) {
+        return dirPath + filePath;
+    });
+    readMultipleFiles(filesPath, 'utf8', function (err, results) {
+        if (err)
+            throw err;
         console.log(results);
     });
 });
