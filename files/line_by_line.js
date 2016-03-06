1383c30b11918f2246115415a9a3f7a6e2873b36
Zeeshan Hassan <zishon89us@hotmail.com>
2016-03-06 18:01:43 +0500

    read file line by line

--- files/line_by_line.js
+++ files/line_by_line.js
@@ -1,3 +1,41 @@
 /**
  * Created by Zeeshan on 3/6/2016.
  */
+
+
+
+//------------------------------------------------------
+//read file line by line using module tail
+//Web Link=> http://stackoverflow.com/questions/35823597/nodejs-reading-line-by-line-function-while-loop/35823722#35823722
+//------------------------------------------------------
+/*
+
+ var Tail = require('tail').Tail;
+
+ var tail = new Tail("./sample_files/file1.txt");
+
+ tail.on("line", function(data) {
+ console.log(data);
+ });
+
+ tail.on("error", function(error) {
+ console.log('ERROR: ', error);
+ });
+
+*/
+//------------------------------------------------------
+//read file line by line using module fs and async
+//Web Link=> http://stackoverflow.com/questions/35823597/nodejs-reading-line-by-line-function-while-loop/35823722#35823722
+//------------------------------------------------------
+
+
+    var readline = require('linebyline'),
+        rl = readline('./sample_files/file1.txt');
+    rl.on('line', function (line, lineCount, byteCount) {
+        console.log(lineCount, line, byteCount);
+        // do something with the line of text
+    })
+    .on('error', function (e) {
+            console.log("error", e);
+        // something went wrong
+    });
