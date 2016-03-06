f35c03508bdcfb8c9896869ce3e6b5867d1011d3
Zeeshan Hassan <zishon89us@hotmail.com>
2016-03-06 17:35:42 +0500

    list node.js built-in modules

---
+++ random/tiny_useful_modules/builtin-modules.js
@@ -0,0 +1,9 @@
+
+//------------------------------------------------------
+//list node.js core modules
+//Web Link=> http://stackoverflow.com/questions/35824135/exact-list-of-node-core-modules
+//------------------------------------------------------
+
+var builtinModules = require('builtin-modules');
+
+console.log(builtinModules);
