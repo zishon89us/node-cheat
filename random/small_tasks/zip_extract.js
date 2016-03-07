bd2f06a6d102816a66e35164d9c1d0ef7806e416
Zeeshan Hassan <zishon89us@hotmail.com>
2016-03-07 11:33:22 +0500

    node.js amazing task

---
+++ random/small_tasks/zip_extract.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Zeeshan on 3/7/2016.
+ */
+
+/*
+* download X zip-items, when extracted each zip-item contains Y text-items/image-items
+* Task 1: Create a folder Z if item is image put it in Z or if item is text use links in it to download
+* image file and put it in Z, zip folder Z.
+* Task 2: Create spread-sheet maintaining Y was link or image (next-level provide image size and format info)
+* Task 3: Upload Z folder to GDrive, OneDrive, Dropbox and email respectively for each upload success
+* Task 4: Shoot me sms when all is done!
+* */
