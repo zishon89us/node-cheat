90e08fe319deff85a357ce960f5063871dd6adfb
Zeeshan Hassan <zishon89us@hotmail.com>
2016-03-16 19:33:26 +0500

    process 1000 records in node.js

---
+++ random/small_tasks/1000_records.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Zeeshan on 3/16/2016.
+ */
+
+//------------------------------------------------------
+// Process thousands of rows to calculate distances between
+// points and other small calculations. Script should be written
+// in Node.js, DB is POSTGRESQL. The script should be written in
+// a way that it's easy to add another geolib.
+//
+// Web Link=>
+//------------------------------------------------------
+
