a48a2a7a69ed57d0db411ebcefb41d206e614c6f
Zeeshan Hassan <zishon89us@hotmail.com>
2016-03-28 13:27:48 +0500

    simple express server to send ejs files

--- random/small_tasks/express_server/app.js
+++ random/small_tasks/express_server/app.js
@@ -7,9 +7,12 @@
 //Web Link=> http://stackoverflow.com/questions/36258158/expressjs-view-helpers-do-not-work/36258354#36258354
 //------------------------------------------------------
 
+//USING HTML
+/*
 
 var express = require('express');
 var app = express();
+
 var path = require('path');
 
 // viewed at http://localhost:5000
@@ -25,6 +28,30 @@ function showClientRequest(req, res, next) {
         }
     }
     console.log(request)
+    return next();
+}
+
+app.listen(5000);
+
+*/
+
+
+//USING EJS TEMPLATE ENGINE
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+var path = require('path');
+
+// viewed at http://localhost:5000
+app.get('/', showClientRequest, function(req, res) {
+    res.render('index',{message:"Hello World!"});
+});
+
+function showClientRequest(req, res, next) {
+    console.log('Something Here...');
     return next();
 }
 
