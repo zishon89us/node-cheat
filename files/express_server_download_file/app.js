34fd8ec74277f433059604e7eda5598f09fdbe25
zishon89us <zishon89us@hotmail.com>
2016-04-18 21:35:43 +0500

    download file using express and fs

--- files/express_server_download_file/app.js
+++ files/express_server_download_file/app.js
@@ -3,8 +3,8 @@
  */
 
 //------------------------------------------------------
-//Simple Express Server to send html file to client
-//Web Link=> http://stackoverflow.com/questions/36258158/expressjs-view-helpers-do-not-work/36258354#36258354
+//Simple Express Server to download file
+//Web Link=>
 //------------------------------------------------------
 
 //USING HTML
@@ -17,15 +17,13 @@ var path = require('path'),
     fs = require('fs');
 
 // viewed at http://localhost:5000
-app.get('/', showClientRequest, function(req, res) {
+app.get('/', showClientRequest, function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+//downloads file
+app.get('/download', function (req, res) {
 
-
-app.get('/download', function(req, res){
-
-    //var file = __dirname + '/upload-folder/dramaticpenguin.MOV';
     var file = '.././sample_files/file1.txt';
 
     var filename = path.basename(file);
@@ -40,9 +38,9 @@ app.get('/download', function(req, res){
 
 function showClientRequest(req, res, next) {
     var request = {
-        REQUEST : {
+        REQUEST: {
             HEADERS: req.headers,
-            BODY : req.body
+            BODY: req.body
         }
     }
     console.log(request)
