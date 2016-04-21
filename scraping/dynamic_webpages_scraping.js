c37229d667b864ac7ed4fc0262783e46db5d4c60
zishon89us <zishon89us@hotmail.com>
2016-04-21 13:48:58 +0500

    scraping init

---
+++ scraping/dynamic_webpages_scraping.js
@@ -0,0 +1,12 @@
+
+//------------------------------------------------------
+//Web scraping tool for non-static web pages. Most websites we are interested in consist
+// of a long list of results that load as you scroll down. The solution seems to be
+// automated headless scraping using Selenium Webdriver or PhantomJS.
+
+//Strategy=> The current strategy is to use Node.Js to drive Selenium and PhantomJS.
+//Web Link=> http://stackoverflow.com/questions/18102392/of-scraping-data-headless-browsers-and-python
+//Web Link=> http://docs.seleniumhq.org/
+//Web Link=> http://www.phantomjs.org/
+//------------------------------------------------------
+
