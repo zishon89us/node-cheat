c3a43c1a891beb0e02b76e52317033e03ad87af3
zishon89us <zishon89us@hotmail.com>
2016-04-22 22:06:42 +0500

    one to one chat socket.io

--- chat/simple_chat/index.js
+++ chat/simple_chat/index.js
@@ -1,3 +1,48 @@
-/**
- * Created by Hair on 4/22/2016.
- */
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server),
+    port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+var users = {},
+    sockets = {};
+
+io.on('connection', function (socket) {
+    var addedUser = false;
+
+    //only private messaging is handled
+    socket.on('new message', function (to, data) {
+
+        // send private message by looking at socket info previously saved
+        sockets[users[to]].emit(
+            'message',
+            {
+                message : data,
+                from : sockets[socket.id].username
+            }
+        );
+
+    });
+
+    //handle new user
+    socket.on('add user', function (username) {
+
+        users[username] = socket.id;    //  reference to your socket ID
+        sockets[socket.id] = { username : username, socket : socket };  //  reference to your socket
+
+        // let everyone know
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            onlineUsers: Object.keys(sockets).length
+        });
+
+    });
+
+});
