3bc46861d4fd76fdb946128394dd25705b19a542
zishon89us <zishon89us@hotmail.com>
2016-08-04 21:42:11 +0500

    stripe form to get cc info

--- payments/stripe_one_time_payment/server.js
+++ payments/stripe_one_time_payment/server.js
@@ -14,7 +14,12 @@ var https = require('https'),
     fs = require('fs'),
     config = require('../config'),
     stripe = require('stripe')(config.STRIPE_KEY),
+    ejs = require('ejs'),
+    path = require('path'),
     app = require('express')();
+
+app.set('view engine', 'ejs');
+
 
 var options = {
     key: fs.readFileSync('../../HTTPS/keys/key.pem'),
@@ -22,14 +27,17 @@ var options = {
 };
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    //res.send('Hello World!');
+    res.render('index',{message:"Hello World!"});
 });
 
 //create stripe customer
 app.post('/customer', function (req, res) {
-    stripe.customers.create(
-        { email: req.body.email },
-        function(err, customer) {
+    stripe.customers.create({
+            email: req.query.email || Date.now()+"@nodecheat.com",
+            description: req.query.description || "Desc. for " + Date.now()+"@nodecheat.com" ,
+            source: req.query.source // obtained with Stripe.js
+        }, function(err, customer) {
             if(err){
                 console.log("stripe.customers.create Failed ", err);
                 res.send({message: "stripe.customers.create Failed ", err: err});
@@ -41,10 +49,31 @@ app.post('/customer', function (req, res) {
     );
 });
 
+//get customer with customer_id
+app.post('/customer', function (req, res) {
+    if(req.query.customer_id){
+        stripe.customers.retrieve(req.query.customer_id,
+            function(err, customer) {
+                if(err){
+                    var msg = "stripe.customers.retrieve Failed ";
+                    console.log(msg, err);
+                    res.send({message: msg, err: err});
+                }else{
+                    var msg = "stripe.customers.retrieve Succeeded ";
+                    console.log(msg );
+                    res.send({message: msg, customer: customer});
+                }
+            }
+        );
+    }else{
+        res.send({message:"Params Missing"});
+    }
+});
 
 
-//charge one time using token
-app.post('/charge', function (req, res) {
+
+//charge one time using token (obtained form stripe.js)
+app.post('/charge-immediate', function (req, res) {
     if(req.query.token){
         stripe.charges.create({
             amount: req.query.amount,
@@ -60,7 +89,53 @@ app.post('/charge', function (req, res) {
             }else{
                 var msg = "stripe.charges.create Succeeded ";
                 console.log(msg );
-                res.send({message: msg, carge: charge});
+                res.send({message: msg, charge: charge});
+            }
+        });
+    }else{
+        res.send({message:"Params Missing"});
+    }
+});
+
+//charge customer but don't capture
+app.post('/charge-hold', function (req, res) {
+    if(req.query.customer_id){
+        stripe.charges.create({
+            amount: req.query.amount,
+            currency: req.query.currency || "usd",
+            customer: req.query.customer_id, // obtained with Stripe.js
+            description: req.query.desc,
+            capture: false,
+            statement_descriptor : "Node Cheat",
+            metadata: {'node_cheat_by': 'https://github.com/zishon89us'}
+        }, function(err, charge) {
+            if(err){
+                var msg = "stripe.charges.create Failed ";
+                console.log(msg, err);
+                res.send({message: msg, err: err});
+            }else{
+                var msg = "stripe.charges.create Succeeded ";
+                console.log(msg );
+                res.send({message: msg, charge: charge});
+            }
+        });
+    }else{
+        res.send({message:"Params Missing"});
+    }
+});
+
+//charge one time using token
+app.post('/charge-capture', function (req, res) {
+    if(req.query.charge_id){
+        stripe.charges.capture(req.query.charge_id, function(err, charge) {
+            if(err){
+                var msg = "stripe.charges.capture Failed ";
+                console.log(msg, err);
+                res.send({message: msg, err: err});
+            }else{
+                var msg = "stripe.charges.capture Succeeded ";
+                console.log(msg );
+                res.send({message: msg, charge: charge});
             }
         });
     }else{
@@ -78,6 +153,32 @@ app.get('/charge', function (req, res) {
         res.send({message:"Params Missing"});
     }
 });
+
+
+//Events
+
+//get a charge info with ID and also bring customer associated with ti
+app.get('/event', function (req, res) {
+    if(req.query.event_id){
+        stripe.events.retrieve(req.query.event_id,
+            function(err, event) {
+                if(err){
+                    var msg = "stripe.events.retrieve Failed ";
+                    console.log(msg, err);
+                    res.send({message: msg, err: err});
+                }else{
+                    var msg = "stripe.events.retrieve Succeeded ";
+                    console.log(msg );
+                    res.send({message: msg, event: event});
+                }
+            }
+        );
+    }else{
+        res.send({message:"Params Missing"});
+    }
+});
+
+
 
 https.createServer(options, app).listen(3000, function () {
     console.log('Server Started!');
