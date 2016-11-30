d2ce4cf819402e59fe93b8b25563478aefe90e85
zishon89us <zishon89us@hotmail.com>
2016-12-01 03:03:45 +0500

    multer_s3 answer updated

--- aws/express_multer_s3/app.js
+++ aws/express_multer_s3/app.js
@@ -3,11 +3,63 @@
  */
 
 //------------------------------------------------------
+//NEW-ANSWER @ DEC-2016
 //multi-part direct upload to s3 without saving on local disk
 //Web Link=> https://github.com/expressjs/multer/issues/196
 //Run : node app.js
 //------------------------------------------------------
 
+var express = require('express'),
+    aws = require('aws-sdk'),
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
+    multerS3 = require('multer-s3');
+
+aws.config.update({
+    secretAccessKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    accessKeyId: 'XXXXXXXXXXXXXXX',
+    region: 'us-east-1'
+});
+
+var app = express(),
+    s3 = new aws.S3();
+
+app.use(bodyParser.json());
+
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'bucket-name',
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, file.originalname); //use Date.now() for unique file keys
+        }
+    })
+});
+
+//open in browser to see upload form
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//used by upload form
+app.post('/upload', upload.array('upl',1), function (req, res, next) {
+    res.send("Uploaded!");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+// ------------------------------------------------------
+// OLD-ANSWER
+//multi-part direct upload to s3 without saving on local disk
+//Web Link=> https://github.com/expressjs/multer/issues/196
+//Run : node app.js
+//NOTE ::: use multer@1.1.0 and multer-s3@1.4.1 for following snippet:
+//------------------------------------------------------
+
+/*
 var express = require('express'),
     bodyParser = require('body-parser'),
     multer = require('multer'),
@@ -42,4 +94,4 @@ app.post('/upload', upload.array('upl'), function (req, res, next) {
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-});
+});*/
