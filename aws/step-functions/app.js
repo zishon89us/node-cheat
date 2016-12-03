34add9f0e45469d946aff556dec54762016bfc80
zishon89us <zishon89us@hotmail.com>
2016-12-04 00:27:01 +0500

    aws step-functions init

---
+++ aws/step-functions/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Zeeshan on 12/4/2016.
+ */
+
+
+//------------------------------------------------------
+//New – AWS Step Functions – Build Distributed Applications Using Visual Workflows
+//Web Link=> https://aws.amazon.com/blogs/aws/new-aws-step-functions-build-distributed-applications-using-visual-workflows/
+//Run : node app.js
+//------------------------------------------------------
