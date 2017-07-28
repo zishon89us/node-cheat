0d5bb4441d16084f6f56b8fb0326f42ccb773aa6
zishon89us <zishon89us@hotmail.com>
2017-07-29 01:18:47 +0500

    redis tutorial link

--- redis/redis_windows.js
+++ redis/redis_windows.js
@@ -4,7 +4,8 @@
 
 //------------------------------------------------------
 //Run Redis on windows
-//Web Link=> 1) https://github.com/MicrosoftArchive/redis/releases
-//           2) http://geekindulgence.com/install-redis-as-a-windows-service-using-chocolatey/
+//Web Link=> 1) Download: https://github.com/MicrosoftArchive/redis/releases
+//           2) Download: http://geekindulgence.com/install-redis-as-a-windows-service-using-chocolatey/
+//           3) Quick Tutorial: https://redis.io/topics/data-types-intro
 //Run :
 //------------------------------------------------------
