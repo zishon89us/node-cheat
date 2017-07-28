e8254a3b0c2c8d46dce55190f7b8409d932d8460
zishon89us <zishon89us@hotmail.com>
2017-07-29 01:12:42 +0500

    redis windows download

---
+++ redis/redis_windows.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Zeeshan on 7/29/2017.
+ */
+
+//------------------------------------------------------
+//Run Redis on windows
+//Web Link=> https://github.com/MicrosoftArchive/redis/releases
+//Run :
+//------------------------------------------------------
