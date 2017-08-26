c24d887b34c5efac87c86b3d976141de9c8fe8b0
zishon89us <zishon89us@hotmail.com>
2017-08-26 18:51:40 +0500

    static hosting options available

---
+++ static-hosting/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zeeshan on 8/26/2017.
+ */
+
+
+//------------------------------------------------------
+//static hosting
+//Web Link=> https://designrope.com/toolbox/static-web-hosting/ (shows many options)
+//Web Link=> https://davidwalsh.name/hosting-website-amazon-s3 (s3 way)
+//           http://docs.aws.amazon.com/AmazonS3/latest/dev/WebsiteHosting.html
+//Web Link=> Free static page hosting on google app engine
+//Web Link=> https://surge.sh/ (seems good option and easy too)
+//Web Link=> https://www.ibm.com/developerworks/cloud/library/cl-deploy-a-hello-world-webpage-to-bluemix-app/index.html
+//------------------------------------------------------
