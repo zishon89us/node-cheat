d32c94c8b1f18b6c368369d6676addcc20cdfce3
zishon89us <zishon89us@hotmail.com>
2018-02-28 20:13:10 +0500

    deploy node app on linux 2018 style

---
+++ deployment/2018_style.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Zeeshan on 2/28/2018.
+ */
+
+//------------------------------------------------------
+// The ultimate guide to deploying your node app on Linux
+// By Mike on 12th Feb 2018
+// Web Link=> https://certsimple.com/blog/deploy-node-on-linux
+//------------------------------------------------------
+
+
+// Use above link to tutorial on HOW TO DEPLOY NODE APP ON LINUX
+
+
+//------------------------------------------------------
+// Load Balancing
+// Web Link=> https://certsimple.com/blog/deploy-node-on-linux#node-js-ssl-load-balancing
+//------------------------------------------------------
+
+// For load balancing
