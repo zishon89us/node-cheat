c3d2fe6a1ddbb3c7c259a14be6e8bd0ce5317c52
zishon89us <zishon89us@hotmail.com>
2018-03-27 19:03:44 +0500

    s3 upload es8 style

--- aws/express_multer_s3/app_es8.js
+++ aws/express_multer_s3/app_es8.js
@@ -10,3 +10,52 @@
 //Run : node app.js
 //------------------------------------------------------
 
+const express = require('express');
+const aws = require('aws-sdk');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+aws.config.update({
+    secretAccessKey: 'YOUR_ACCESS_SECRET_KEY',
+    accessKeyId: 'YOUR_ACCESS_KEY_ID',
+    region: 'us-east-1'
+});
+
+const app = express();
+const s3 = new aws.S3();
+
+app.use(bodyParser.json());
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        acl: 'public-read',
+        bucket: 'YOUR_BUCKET_NAME',
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, file.originalname); //use Date.now() for unique file keys
+        }
+    })
+});
+
+//open http://localhost:3000/ in browser to see upload form
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//used by upload form
+app.post('/upload', upload.array('upl',1), (req, res, next) => {
+    res.send("Uploaded!");
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+
+//------------------------------------------------------
+//NEW-ANSWER @ DEC-2016 [ES5 STYLE]
+//------------------------------------------------------
+
+/* SEE app.js in current directory Line#13*/
