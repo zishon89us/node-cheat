78f62f97d3a33ed936d1d11074df8aefcbfa285f
zishon89us <zishon89us@hotmail.com>
2018-03-27 18:59:15 +0500

    s3 upload es8 style

---
+++ aws/express_multer_s3/app_es8.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Zeeshan on March 27, 2018.
+ */
+
+//------------------------------------------------------
+//LATEST-ANSWER @ March-2018 [ES8 STYLE]
+//multi-part direct upload to s3 without saving on local disk
+//Web Link=> http://stackoverflow.com/a/35902286/3539857
+//OTHER IMPLEMENTATIONS=> see app.js in current directory
+//Run : node app.js
+//------------------------------------------------------
+
