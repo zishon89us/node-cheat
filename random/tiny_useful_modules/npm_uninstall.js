3a0f471d2c735bd79b4bed05cb7bd07065ac373d
zeeshanhassanpk <zishon89us@hotmail.com>
2019-08-11 22:44:45 +0500

    remove all packages globally

--- random/tiny_useful_modules/npm_uninstall.js
+++ random/tiny_useful_modules/npm_uninstall.js
@@ -1,16 +1,7 @@
 
 //------------------------------------------------------
-//Capture Screen-shot of websites
-//Web Link=>
+// How to remove all npm modules globally?
+// Web Link=> https://stackoverflow.com/questions/9283472/command-to-remove-all-npm-modules-globally/19357869
 //------------------------------------------------------
 
-var Pageres = require('pageres');
-
-var pageres = new Pageres({delay: 2})
-    //.src('github.com/zishon89us/node-cheat', ['1280x1024', '480x320', '1024x768', 'iphone 5s'], {crop: true})
-    .src('github.com/zishon89us/node-cheat', ['1920x1080']) //creates on capture only
-    .dest(__dirname + "/screenshot_dest") //destination folder to store captures
-    .run()
-    .then(function () {
-        console.log('Screen-shots are done!');
-    });
+// > npm ls -gp --depth=0 | awk -F/ '/node_modules/ && !/\/npm$/ {print $NF}' | xargs npm -g rm
