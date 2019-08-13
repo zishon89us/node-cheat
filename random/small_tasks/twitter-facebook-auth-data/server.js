1a7edd83ce0db80f567563298905b4b037f27297
zeeshanhassanpk <zishon89us@hotmail.com>
2019-08-13 19:13:30 +0500

    fixed warnings deprecated

--- random/small_tasks/twitter-facebook-auth-data/server.js
+++ random/small_tasks/twitter-facebook-auth-data/server.js
@@ -25,12 +25,15 @@ require('./config/passport')(passport); // pass passport for configuration
 // set up our express application
 app.use(logger('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: false })); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilikenodecheat' })); // session secret
+app.use(session({
+    secret: 'ilikenodecheat', resave: false,
+    saveUninitialized: true,
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
