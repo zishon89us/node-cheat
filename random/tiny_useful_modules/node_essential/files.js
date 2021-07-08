88ce0acd6b99bb730eef34e1671600af38a354a2
Zeeshan Hassan Memon <zishon89us@hotmail.com>
2021-07-09 04:26:20 +0500

    node essentials > files create, read, rename, copy, delete

--- random/tiny_useful_modules/node_essential/files.js
+++ random/tiny_useful_modules/node_essential/files.js
@@ -24,5 +24,27 @@ fs.writeFileSync(path.join(__dirname, 'sample_files', files[0]), 'Just a new lin
 
 fs.appendFile(path.join(__dirname, 'sample_files', files[1]), `\nJust a new line appended`, (err, data) => {
 	console.log(data);
-})
+});
 
+const newFile = 'file3.txt';
+const newFilePath = path.join(__dirname, 'sample_files', newFile);
+const newFilePathCopy = path.join(__dirname, 'sample_files', '_' + newFile);
+
+// create a new file on the fly
+fs.writeFileSync(newFilePath, 'This is new file.');
+
+// read it
+const newFileContent = fs.readFileSync(newFilePath, 'utf-8');
+console.log('New File Content: ', newFilePath);
+
+// rename it
+fs.renameSync(newFilePath, newFilePathCopy)
+
+// copy it
+fs.copyFileSync(newFilePathCopy, newFilePath)
+
+// move it
+fs.renameSync(newFilePath, path.join(__dirname, 'sample_files_copy', '__' + newFile))
+
+// delete it
+fs.unlinkSync(newFilePathCopy);
